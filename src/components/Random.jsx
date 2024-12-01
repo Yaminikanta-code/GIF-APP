b1c57da933fa7bf3de59a553dd87d5ed6f424fc0
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-02 00:01:25 +0530

    add code

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import axios from "axios";
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Random() {
   useEffect(() => {
