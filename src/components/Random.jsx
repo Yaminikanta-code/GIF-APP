389acee40dd83e291350e62043b823fd127261bf
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-01 23:53:53 +0530

    add code

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,14 +1,12 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import axios from "axios";
-import.meta.env.REACT_APP_API_KEY;
-const API_KEY = "process.env.REACT_APP_API_KEY";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Random() {
   useEffect(() => {
     async function getGif() {
-      const url =
-        "https://api.giphy.com/v1/gifs/random?api_key=J6MbP5UjiEz8tSDhxV5WzwAAijYQ34CP";
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const { data } = await axios.get(url);
       console.log(data);
     }
