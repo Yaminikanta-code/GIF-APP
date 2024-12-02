16d21b940cd5050c5a2ca0b6083a39a8123e1b39
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-02 23:46:05 +0530

    add code

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -4,17 +4,21 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Random() {
+  const [gif, setGif] = useState("");
   useEffect(() => {
     async function getGif() {
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const { data } = await axios.get(url);
-      console.log(data);
+      const imageSource = data.data.images.downsized_large.url;
+      //console.log(imageSource);
+      setGif(imageSource);
     }
     getGif();
   }, []);
   return (
     <>
-      <h1>Random</h1>
+      <h1>Random GIF</h1>
+      <img width="500" src={gif} alt="Ramdom GIF" />
     </>
   );
 }
