6fa174b1a7a2290bd788d5e80f82bf79fa54cb56
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-02 23:59:36 +0530

    add code

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -5,22 +5,26 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Random() {
   const [gif, setGif] = useState("");
-  useEffect(() => {
-    async function getGif() {
+   async function getGif() {
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const { data } = await axios.get(url);
       const imageSource = data.data.images.downsized_large.url;
       //console.log(imageSource);
       setGif(imageSource);
     }
+  useEffect(() => {
     getGif();
   }, []);
+  const handleClick = () => {
+    getGif();
+  };
+  
   return (
     <>
       <div className="container">
         <h1>Random GIF</h1>
-        <img width="500" height={"500"} src={gif} alt="Ramdom GIF" />
-        <button>Random GIF</button>
+        <img width="500" height={} src={gif} alt="Ramdom GIF" />
+        <button onClick={handleClick}>Random GIF</button>
       </div>
     </>
   );
